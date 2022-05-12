410d45b738372140776a5faf5fcc06e829a1d39a
InYoung Park <dlsdudg15@naver.com>
2022-05-12 22:02:05 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
@@ -0,0 +1,11 @@
+s, k, h = map(int, input().split())
+
+if s + k + h >= 100:
+    print('OK')
+else:
+    if min(s, k, h) == s:
+        print('Soongsil')
+    elif min(s, k, h) == k:
+        print('Korea')
+    else:
+        print('Hanyang')
