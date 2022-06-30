57de78f395c6e4ad4d7fb5fc8d26b1c50e7b1239
InYoung Park <dlsdudg15@naver.com>
2022-06-30 21:39:52 +0900

    [Bronze IV] Title: Äpplen och päron, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/21354. Äpplen och päron/Äpplen och päron.py
@@ -0,0 +1,10 @@
+apple, pear = map(int, input().split())
+axel = apple * 7
+petra = pear * 13
+
+if axel > petra:
+    print("Axel")
+elif axel < petra:
+    print("Petra")
+else:
+    print("lika")
