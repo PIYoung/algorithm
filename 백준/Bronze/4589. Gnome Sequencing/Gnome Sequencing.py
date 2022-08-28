77cb33b72e01d6ab64afaf4e5b7ba590750bb330
InYoung Park <dlsdudg15@naver.com>
2022-08-29 06:44:31 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
@@ -1,11 +1,34 @@
-N = int(input())
+def gnomes_is_ordered_or_not(gnomes):
+    is_ordered = False
+    order_check_list = []
+    for idx in range(len(gnomes) - 1):
+        check_num = gnomes[idx] - gnomes[idx+1]
+        order_check_list.append(check_num < 0)
+        
+    if len(set(order_check_list)) == 1:
+        is_ordered = True
+        
+    return is_ordered
+        
 
-result = ["Gnomes:"]
-for i in range(N):
-    beard_0, beard_1, beard_2 = map(int, input().split(" "))
-    if (beard_1 - beard_0) * (beard_2 - beard_1) > 0:
-        result.append("Ordered")
-    else:
-        result.append("Unordered")
+def gnome_sequencing(gnomes_list):
+    print("Gnomes:")
+    for gnomes in gnomes_list:
+        is_ordered = gnomes_is_ordered_or_not(gnomes)
+        
+        if is_ordered:
+            print("Ordered")
+        else:
+            print("Unordered")
 
-print("\n".join(result))
+            
+if __name__ == "__main__":
+    gnomes_list = []
+    
+    N = int(input())
+    
+    for _ in range(N):
+        gnomes = list(map(int, input().split()))
+        gnomes_list.append(gnomes)
+        
+    gnome_sequencing(gnomes_list)
