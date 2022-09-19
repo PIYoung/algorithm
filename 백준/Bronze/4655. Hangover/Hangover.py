5243a71aae5565b2a8a943885411301a424665ba
InYoung Park <dlsdudg15@naver.com>
2022-09-19 09:21:26 +0900

    [Bronze III] Title: Hangover, Time: 120 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4655. Hangover/Hangover.py
@@ -0,0 +1,14 @@
+while True:
+    n = float(input())
+    
+    if n == 0:
+        break
+        
+    i = 2
+    t = 0
+    
+    while t < n:
+        t += 1 / i
+        i += 1
+        
+    print(f"{i - 2} card(s)")
